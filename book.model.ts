8f12f2f740c3be01198cd96919dafd6d51e60b4c
pranya1 <47565102+pranya1@users.noreply.github.com>
2019-06-04 10:09:00 +0530

    Add files via upload

---
+++ book.model.ts
@@ -0,0 +1,13 @@
+export class BookDetails {
+   bookId: number ;
+	publishDate: Date ;
+    isbnNumber: number ;
+	title: String ;
+	author: String ;
+	averageRating: number ;
+	numberOfReviews: number ;
+	price: number ;
+	bookImage: String ;
+	description: String ;
+	category: String ;
+}
